df6373de18522f6b5d84263d4aeee6f1af82dd82
Terrick Thibodeaux <terrickthibodeaux@gmail.com>
2021-09-10 09:36:06 -0500

    Added a array that goes through it.

---
+++ Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Array
+{
+    class Game
+    {
+        public void Run()
+        {
+           
+            //Prints all numbers in a Array
+            int[] arr = new int[6] { 1, 12, 18, 58, 69, 72 };
+            PrintArray(arr);
+        }
+        //Varibles
+        int input = 0;
+        
+        void PrintArray(int[]arr)
+        {
+            for (int i=0; i<arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+        void PrintSmallLarge()
+        {
+
+        }
+    }
+}
