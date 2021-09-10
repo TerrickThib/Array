04bbe16658dfdf5f9b8acea229ff8eff62a71302
Terrick Thibodeaux <terrickthibodeaux@gmail.com>
2021-09-10 10:14:55 -0500

    Added a Fuction that finds my LArgest and Smallest Number in a Array

--- Game.cs
+++ Game.cs
@@ -8,9 +8,10 @@ namespace Array
     {
         public void Run()
         {
-           
+            
             //Prints all numbers in a Array
-            int[] arr = new int[6] { 1, 12, 18, 58, 69, 72 };
+            int[] arr = new int[] { 1, 12, 18, 58, 69, 72 };
+            PrintSmallLarge(arr);
             PrintArray(arr);
         }
         //Varibles
@@ -23,9 +24,26 @@ namespace Array
                 Console.WriteLine(arr[i]);
             }
         }
-        void PrintSmallLarge()
+        void PrintSmallLarge(int[]arr)
         {
+            //VAribles for the smallest and largest numbers
+            int largest = arr[0];
+            int smallest = arr[0];
+            //This will cycle through the array by one.It also finds the Largest and smallest numbers
+            for (int i = 0; i<arr.Length; i++)
+            {
+                if(arr[i]>largest)
+                {
+                    largest = arr[i];
+                }
 
+                else if(arr[i]<smallest)
+                {
+                    smallest = arr[i];
+                }
+            }
+            Console.WriteLine("Largest; " +largest);
+            Console.WriteLine("Smallest: " +smallest);
         }
     }
 }
